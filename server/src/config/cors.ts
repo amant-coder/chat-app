bbb58abf5a7dd3096bc4409fb0a64e90ef31c0b9
amant-creator <at0068812@gmail.com>
2026-04-16 00:52:50 +0530

    updated

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -2,9 +2,8 @@ import cors from 'cors';
 import { env } from './env';
 
 export const corsOptions: cors.CorsOptions = {
-  origin: env.CLIENT_URL,
+  origin: env.CLIENT_URL.replace(/\/$/, ''),
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  allowedHeaders: ['Content-Type', 
-    'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
